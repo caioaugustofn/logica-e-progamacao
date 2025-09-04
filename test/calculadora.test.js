e5d72b0e4c32b4e478a9b0e4ced42f18bbd0617b
Caio <caufrano@gmail.com>
2025-09-04 17:04:41 -0300

    Adicionei 2 testes para minha função

--- test/calculadora.test.js
+++ test/calculadora.test.js
@@ -11,4 +11,12 @@ describe("Testes da Função de Soma", function () {
     const resultadoDaSoma = somarDoisNumeros(50, -15);
     expect(resultadoDaSoma).to.equal(35);
   });
+  it("A função deve ser capaz de somar 2 numeros zerados", function () {
+    const resultadoDaSoma = somarDoisNumeros(0, 0);
+    expect(resultadoDaSoma).to.equal(0);
+  });
+  it("A função deve ser capaz de somar 2 numeros negativos", function () {
+    const resultadoDaSoma = somarDoisNumeros(-25, -850);
+    expect(resultadoDaSoma).to.equal(-875);
+  });
 });
